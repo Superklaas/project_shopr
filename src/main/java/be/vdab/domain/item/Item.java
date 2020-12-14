2531bf2eb6f175d148ad73d80412ec4804b10a7c
Maxim V <maximverbeeck@hotmail.com>
2020-12-14 13:23:11 +0100

    zoveelste poging

--- src/main/java/be/vdab/domain/item/Item.java
+++ src/main/java/be/vdab/domain/item/Item.java
@@ -4,7 +4,6 @@ import javax.persistence.*;
 
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
-@DiscriminatorColumn(name = "item_type", discriminatorType = DiscriminatorType.STRING)
 public abstract class Item {
 
     @Id
