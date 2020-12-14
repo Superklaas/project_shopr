5ca0a6eeb79a059ce5b2ef8e2407087a0800d774
Klaas Decock <klaasdc@icloud.com>
2020-12-14 17:38:59 +0100

    crud methods ItemRepository

---
+++ src/main/java/be/vdab/repository/ItemRepository.java
@@ -0,0 +1,44 @@
+package be.vdab.repository;
+
+import be.vdab.domain.item.Book;
+import be.vdab.domain.item.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+public interface ItemRepository extends JpaRepository<Item,Integer> {
+
+    default void createItem(Item item) {
+        save(item);
+    }
+
+    @Query("select i from Item i order by i.title, i.price asc")
+    List<Item> getAllItems();
+
+    @Query("select b from Book b order by b.title, b.author, b.price asc")
+    List<Book> getAllBooks();
+
+    @Query("select b from Fiction b order by b.title, b.author, b.price asc")
+    List<Book> getAllFictions();
+
+    @Query("select b from NonFiction b order by b.title, b.author, b.price asc")
+    List<Book> getAllNonFictions();
+
+    @Query("select g from Game g order by g.title, g.publisher, g.price asc")
+    List<Book> getAllGames();
+
+    @Query("select l from LP l order by l.title, l.artist, l.price asc")
+    List<Book> getAllLPs();
+
+    default void updateItem(Item item) {
+        save(item);
+    }
+
+    default void deleteItem(Item item) {
+        delete(item);
+    }
+
+}
